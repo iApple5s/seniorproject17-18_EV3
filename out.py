ad6b869c430a95048fb440a09d675b2e50217968
iApple5s <caleb.meyer0@gmail.com>
2018-04-26 16:05:49 -0500

    Example output file modification
    Minor changes to output .py example files to reflect latest changes to program, as well as addition of example.py, which was generated during the in-class presentation of the project.

--- out.py
+++ out.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
+import ev3dev.ev3 as ev3
 from ev3dev.ev3 import *
 from time import sleep
 motorL = LargeMotor('outB')
 motorR = LargeMotor('outC')
-motorL.run_to_rel_pos(position_sp=3119, speed_sp=+300, stop_action='hold')
-motorR.run_to_rel_pos(position_sp=3119, speed_sp=+300, stop_action='hold')
+motorL.run_to_rel_pos(position_sp=312, speed_sp=+500, stop_action='hold')
+motorR.run_to_rel_pos(position_sp=312, speed_sp=+500, stop_action='hold')
 motorR.wait_while('running')
-RotateMotor(OUT_B, 300, 357);
 
-motorL.run_to_rel_pos(position_sp=624, speed_sp=+300, stop_action='hold')
-motorR.run_to_rel_pos(position_sp=624, speed_sp=+300, stop_action='hold')
+motorL.run_to_rel_pos(position_sp=-178, speed_sp=+500, stop_action='hold')
+motorR.run_to_rel_pos(position_sp=178, speed_sp=+500, stop_action='hold')
 motorR.wait_while('running')
-Sound.speak("Done")
+
+motorL.run_to_rel_pos(position_sp=312, speed_sp=+500, stop_action='hold')
+motorR.run_to_rel_pos(position_sp=312, speed_sp=+500, stop_action='hold')
+motorR.wait_while('running')
+
+motorL.run_to_rel_pos(position_sp=357, speed_sp=+500, stop_action='hold')
+motorR.run_to_rel_pos(position_sp=-357, speed_sp=+500, stop_action='hold')
+motorR.wait_while('running')
+
+motorL.run_to_rel_pos(position_sp=624, speed_sp=+500, stop_action='hold')
+motorR.run_to_rel_pos(position_sp=624, speed_sp=+500, stop_action='hold')
+motorR.wait_while('running')
+
+
+motorL.run_to_rel_pos(position_sp=12474, speed_sp=+500, stop_action='hold')
+motorR.run_to_rel_pos(position_sp=12474, speed_sp=+500, stop_action='hold')
+motorR.wait_while('running')
+
